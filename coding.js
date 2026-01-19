0ec6ece6d10c4d9d449c9cb5e859c1b5e7d4afc4
aadhitad <168607543+aadhitad@users.noreply.github.com>
2026-01-19 03:34:04 -0800

    js file

---
+++ coding.js
@@ -0,0 +1,27 @@
+let messageIndex = 0;
+
+const messages = [
+  "Are you sure?",
+  "Really sure?",
+  "Think again 😏",
+  "Last chance!",
+  "Okay fine 😭"
+];
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  const currentSize = parseFloat(
+    window.getComputedStyle(yesButton).fontSize
+  );
+
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
